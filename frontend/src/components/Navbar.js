b09bbae911b4a1e4198e820fe502fac180724611
nick <nick@devbox.local.org>
2024-10-17 14:30:33 -0400

    Nav updates

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const isLoggedIn = !!localStorage.getItem('jwtToken'); // Check if the token is present
+
   return (
     <nav>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
+        {!isLoggedIn && <li><Link to="/login">Login</Link></li>}
+        {!isLoggedIn && <li><Link to="/register">Register</Link></li>}
         <li><Link to="/game-night/1">Game Night</Link></li>
+        {isLoggedIn && <li><Link to="/profile">Profile</Link></li>} {/* Conditionally render the profile link */}
+        {isLoggedIn && <li><Link to="/logout">Logout</Link></li>} {/* Conditionally render the logout link */}
       </ul>
     </nav>
   );
