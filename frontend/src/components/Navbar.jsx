3cae377a79d3ce3cf6f6cf81cbcfa269d846e4f9
nick <nick@szum.io>
2024-11-03 11:27:29 -0500

    Update nav layout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,15 +4,22 @@ import { Link } from 'react-router-dom';
 const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
   return (
     <nav className="bg-gray-800 p-4">
-      <ul className="flex space-x-4 list-none p-0">
-        <li><Link to="/" className="text-white">Home</Link></li>
-        {!isLoggedIn && <li><Link to="/login" className="text-white">Login</Link></li>}
-        {!isLoggedIn && <li><Link to="/register" className="text-white">Register</Link></li>}
-        {isLoggedIn && <li><Link to="/games" className="text-white">Games</Link></li>}
-        {isLoggedIn && <li><Link to="/profile" className="text-white">Profile</Link></li>}
-        {isLoggedIn && <li><Link to="/friends" className="text-white">Friends</Link></li>}
-        {isLoggedIn && <li><Link to="/logout" className="text-white">Logout</Link></li>}
-      </ul>
+      <div className="container mx-auto flex justify-between">
+        {/* Left side navigation */}
+        <ul className="flex space-x-4 list-none p-0">
+          <li><Link to="/" className="text-white hover:text-gray-300">Home</Link></li>
+          {isLoggedIn && <li><Link to="/games" className="text-white hover:text-gray-300">Games</Link></li>}
+          {isLoggedIn && <li><Link to="/friends" className="text-white hover:text-gray-300">Friends</Link></li>}
+        </ul>
+
+        {/* Right side navigation */}
+        <ul className="flex space-x-4 list-none p-0">
+          {isLoggedIn && <li><Link to="/profile" className="text-white hover:text-gray-300">Profile</Link></li>}
+          {!isLoggedIn && <li><Link to="/login" className="text-white hover:text-gray-300">Login</Link></li>}
+          {!isLoggedIn && <li><Link to="/register" className="text-white hover:text-gray-300">Register</Link></li>}
+          {isLoggedIn && <li><Link to="/logout" className="text-white hover:text-gray-300">Logout</Link></li>}
+        </ul>
+      </div>
     </nav>
   );
 };
