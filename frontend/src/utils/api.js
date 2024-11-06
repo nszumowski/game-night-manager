6aefdf3d8b04a25e53a149121514a074939ef118
nick <nick@szum.io>
2024-11-05 19:40:28 -0500

    Update docker files

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://192.168.0.133:5000/api',
+  baseURL: import.meta.env.APP_API_URL || 'http://192.168.0.133:5000/api',
   headers: {
     'Content-Type': 'application/json'
   }
