1a3b4e8880674e379cc8d856ccc2ed0114a38f65
nick <nick@szum.io>
2024-11-08 09:08:13 -0500

    Add temp 'logo' to nav and favicon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,10 +4,14 @@ import { Link } from 'react-router-dom';
 const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
   return (
     <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="flex items-center flex-shrink-0">
+          <img src="/die-icon.png" alt="Game Night Manager Logo" className="h-8 w-8 mr-2" />
+          <span className="text-white hover:text-gray-300 whitespace-nowrap">Game Night Manager</span>
+        </Link>
         {/* Left side navigation */}
         <ul className="flex space-x-4 list-none p-0">
-          <li><Link to="/" className="text-white hover:text-gray-300">Home</Link></li>
+          {/* <li><Link to="/" className="text-white hover:text-gray-300">Home</Link></li> */}
           {isLoggedIn && <li><Link to="/games" className="text-white hover:text-gray-300">Games</Link></li>}
           {isLoggedIn && <li><Link to="/friends" className="text-white hover:text-gray-300">Friends</Link></li>}
         </ul>
